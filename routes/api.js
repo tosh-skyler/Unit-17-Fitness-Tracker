b42d17b1f4ccb5410997b0fbb504152be36b916c
Skyler Tosh <skylerjtosh@gmail.com>
2020-05-10 05:30:55 -0700

    Adding saving npm packages, updating server.js file, api.js, will not work due to workout.js not being integrated

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const Workout = require('../models/workout.js');
+
+router.get('/exercise', (req, res) => {
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router; 
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
-// const logger = require('morgan');
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const app = express();
+const Workout = require("models/workout.js");
+
+app.use(logger("dev"));
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -12,11 +15,15 @@ const db = require("./config/keys").mongoURI;
 
 // CONNECT TO MONGO
 mongoose
-	.connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
-  	.then(() => console.log("M O N G O D B   C O N N E C T E D . . ."))
-	  .catch((err) => console.log("[ E R R O R ]: " + err));
-	  
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log("M O N G O D B   C O N N E C T E D . . ."))
+  .catch((err) => console.log("[ E R R O R ]: " + err));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`S E R V E R   S T A R T E D   O N   P O R T ${port}`));
+app.use(require("./routes/routes.js"));
+app.use(require("./routes/api.js"));
+
+app.listen(port, () =>
+  console.log(`S E R V E R   S T A R T E D   O N   P O R T ${port}`)
+);
